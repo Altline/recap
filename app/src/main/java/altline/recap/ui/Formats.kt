56772418450012b5b883511342e236d3e9192184
Dominik Živko <domy.zivko@gmail.com>
2021-09-15 23:26:10 +0200

    Separate date-time format locale from device locale

--- app/src/main/java/altline/recap/ui/Formats.kt
+++ app/src/main/java/altline/recap/ui/Formats.kt
@@ -2,5 +2,10 @@ package altline.recap.ui
 
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
+import java.util.*
+
+// TODO make available in settings
+var dateTimeLocale: Locale = Locale.forLanguageTag("hr")
 
 val DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+    .withLocale(dateTimeLocale)
